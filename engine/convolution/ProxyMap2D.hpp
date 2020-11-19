e662b49d773fe506316fb101b763885f157c3342
Глухманюк Максим Александрович <MGNeo@yandex.ru>
2020-11-19 21:32:41 +0300

    cnn::engine::convolution::ProxyMap2D has been added.

---
+++ engine/convolution/ProxyMap2D.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include "Map2D.hpp"
+
+namespace cnn
+{
+  namespace engine
+  {
+    namespace convolution
+    {
+      template <typename T>
+      class ProxyMap2D
+      {
+
+        static_assert(std::is_floating_point<T>::value);
+
+      public:
+
+        ProxyMap2D(Map2D<T>& map);
+
+        ProxyMap2D(const ProxyMap2D& proxyMap);
+
+        ProxyMap2D(ProxyMap2D&& proxyMap) = delete;
+
+        ProxyMap2D& operator=(const ProxyMap2D& proxyMap) = delete;
+
+        ProxyMap2D& operator=(ProxyMap2D&& proxyMap) = delete;
+
+        Size2D<size_t> GetSize() const noexcept;
+
+        T GetValue(const size_t x, const size_t y) const;
+
+        // Exception guarantee: strong for this.
+        void SetValue(const size_t x, const size_t y, const T value);
+
+        // It clears the state without changing of the topology.
+        void Clear() noexcept;
+
+      private:
+
+        Map2D<T>& Map;
+
+      };
+
+      template <typename T>
+      ProxyMap2D<T>::ProxyMap2D(Map2D<T>& map)
+        :
+        Map{ map }
+      {
+      }
+
+      template <typename T>
+      ProxyMap2D<T>::ProxyMap2D(const ProxyMap2D& proxyMap)
+        :
+        Map{ proxyMap.Map }
+      {
+      }
+
+      template <typename T>
+      Size2D<size_t> ProxyMap2D<T>::GetSize() const noexcept
+      {
+        return Map.GetSize();
+      }
+
+      template <typename T>
+      T ProxyMap2D<T>::GetValue(const size_t x, const size_t y) const
+      {
+        return Map.GetValue(x, y);
+      }
+
+      template <typename T>
+      void ProxyMap2D<T>::SetValue(const size_t x, const size_t y, const T value)
+      {
+        Map.SetValue(x, y, value);
+      }
+
+      template <typename T>
+      void ProxyMap2D<T>::Clear() noexcept
+      {
+        Map.Clear();
+      }
+    }
+  }
+}
--- engine/main.cpp
+++ engine/main.cpp
@@ -5,6 +5,7 @@
 #include "common/ProxyMap.hpp"
 
 #include "convolution/Map2D.hpp"
+#include "convolution/ProxyMap2D.hpp"
 
 #include "convolution/Core2D.hpp"
 #include "convolution/ProxyCore2D.hpp"
@@ -24,6 +25,7 @@ int main(int argc, char** argv)
   cnn::engine::common::ProxyMap<float> proxyMap{ map };
 
   cnn::engine::convolution::Map2D<float> map2D;
+  cnn::engine::convolution::ProxyMap2D<float> proxyMap2D{ map2D };
 
   cnn::engine::convolution::Core2D<float> core;
   cnn::engine::convolution::ProxyCore2D<float> proxyCore{ core };
